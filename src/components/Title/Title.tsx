d2152d5467eb743a0be34a40f66094bb83d8650e
tranjm4 <98729052+tranjm4@users.noreply.github.com>
2023-09-07 07:25:44 -0700

    Cleaned up Title styling

--- src/components/Title/Title.tsx
+++ src/components/Title/Title.tsx
@@ -4,99 +4,94 @@ import Reveal from "../Animations/Reveal";
 import githubLogo from "../../assets/links/github.svg";
 import linkedinLogo from "../../assets/links/linkedin.svg";
 
+const TitleText: React.FC = () => {
+    return (
+        <SlideAppear className="" offsetX="-20vw" offsetY="-10vh" once={false}>
+
+            <Reveal className="">
+                <div className="bg-gradient-to-b from-teal to-purple-400 bg-clip-text text-transparent">
+
+                    <h1 className="text-5xl font-extrabold mb-2
+                   md:text-7xl md:mb-6 xl:text-9xl">
+
+                        I'm <br /> Jonathan Tran
+                    </h1>
+
+                    <h2 className="text-3xl font-extrabold mb-2
+                   md:text-5xl md:mb-4
+                   lg:text-6xl lg:mb-6 xl:text-7xl">
+                        Software Engineer
+                    </h2>
+                    <h3 className="text-xl font-bold mb-2
+                   md:text-3xl md:mb-4
+                   lg:text-4xl lg:mb-6 xl:text-5xl">
+                        University of California, Irvine <br />
+                        <span className=" text-sm md:text-lg lg:text-2xl">
+                            (Sept 2021 - Dec 2024)
+                        </span>
+                    </h3>
+                    <h3 className="text-xl font-bold
+                   md:text-3xl
+                   lg:text-4xl xl:text-5xl">
+                        Computer Science, B.S. <br />
+                        Statistics, Minor
+                    </h3>
+                </div>
+            </Reveal>
+
+        </SlideAppear>
+    )
+}
+
+const Image: React.FC = () => {
+    return (
+        <SlideAppear className="" offsetX="20vw" offsetY="5vh" once={false}>
+            <motion.div
+                className="flex justify-center"
+                whileHover={{ scale: 1.1 }}
+                whileTap={{ scale: 0.9 }}
+                transition={{ type: "spring", damping: 20, stiffness: 200, mass: 2 }}>
+                <div className="relative flex overflow-hidden rounded-3xl aspect-square max-w-[200px] w-[60vw] shadow-3xl
+                                    md:w-[50%] md:max-w-[500px] lg:w-[100%] lg:max-w-[400px]">
+                    {/* Image */}
+                    <img src="/portrait2.jpg" alt="Portrait of me at Figueroa Terrace and Centennial Street in L.A."
+                        className="h-full w-full object-cover"
+                    />
+                </div>
+            </motion.div>
+        </SlideAppear>
+    )
+}
+
+const MediaLinks: React.FC = () => {
+    return (
+        <div className="flex justify-center w-[80%] h-fit z-20
+                                md:w-40">
+            <a href="https://github.com/tranjm4" target="_blank"
+                className="p-1 mr-5 flex justify-center rounded-xl duration-300 hover:bg-dark-700">
+                <img src={githubLogo} alt="Github Logo"
+                    className="w-[20vw] max-w-[100px] lg:max-w-[120px] xl:max-w-[150px]" />
+            </a>
+            <a href="https://www.linkedin.com/in/jonathan-tran-b405301ba/" target="_blank"
+                className="px-4 ml-5 flex justify-center rounded-xl duration-300 hover:bg-dark-700">
+                <img src={linkedinLogo} alt="LinkedIn Logo"
+                    className="w-[15vw] max-w-[80px] lg:max-w-[100px] xl:max-w-[120px]" />
+            </a>
+        </div>
+    )
+}
+
 const Title = () => {
     return (
-        <div className="flex flex-col min-h-screen h-fit md:flex-row md:justify-center mb-[30vh] py-[10vh] overflow-hidden">
-            {/* Title text */}
-            <div className="flex flex-grow justify-center items-center md:flex-grow xl:flex-grow-0 mx-0 md:mx-0 lg:ml-10 lg:mr-0">
-                <div className="relative flex flex-col items-center h-fit w-fit p-10 xl:p-0"
-                >
-                    <div className="w-full h-full">
-
-                        {/* <TextBoxReveal className="absolute w-full h-full top-[-5%] left-[0%] lg:top-[-10%] lg:left-[-5%] hover:scale-110
-                            bg-gradient-to-br from-dark-800 to-transparent via-transparent via-70%" initialScale="0.3" offsetX="-20vw" offsetY="20vh" />
-                        <TextBoxReveal className="absolute w-full h-full top-[0%] left-[5%] lg:top-[0%] lg:left-[5%] hover:scale-110
-                            bg-gradient-to-br from-dark-800 to-transparent via-transparent via-70%" initialScale="2" offsetX="30vw" offsetY="-30vh" />
-                        <TextBoxReveal className="absolute w-full h-full top-[5%] left-[10%] lg:top-[10%] lg:left-[15%] hover:scale-110
-                            bg-gradient-to-br from-dark-800 to-transparent via-transparent via-70%" initialScale="0.5" offsetX="50vw" offsetY="-20vh" /> */}
-
-                        <SlideAppear className="flex flex-col w-full h-full lg:p-5 xl:p-16" offsetX="-20vw" offsetY="-10vh" once={false}>
-
-                            <Reveal className="h-full w-full">
-                                <div className="bg-gradient-to-b from-teal to-purple-400 bg-clip-text text-transparent">
-
-                                    <h1 className="text-5xl font-extrabold mb-2
-                                   md:text-7xl md:mb-6
-                                   lg:text-8xl">
-
-                                        I'm <br /> Jonathan Tran
-                                    </h1>
-
-                                    <h2 className="text-3xl font-extrabold mb-2
-                                   md:text-5xl md:mb-4
-                                   lg:text-6xl lg:mb-6">
-                                        Software Engineer
-                                    </h2>
-                                    <h3 className="text-xl font-bold mb-2
-                                   md:text-3xl md:mb-4
-                                   lg:text-4xl lg:mb-6">
-                                        University of California, Irvine <br />
-                                        <span className=" text-sm md:text-lg lg:text-2xl xl:text-3xl">
-                                            (Sept 2021 - Dec 2024)
-                                        </span>
-                                    </h3>
-                                    <h3 className="text-xl font-bold
-                                   md:text-3xl
-                                   lg:text-4xl">
-                                        Computer Science, B.S. <br />
-                                        Statistics, Minor
-                                    </h3>
-                                </div>
-                            </Reveal>
-
-                        </SlideAppear>
-                    </div>
-                </div>
-
+        <div className="flex flex-col items-center justify-center h-[100vh] w-full py-24 px-5 lg:px-10 [&>*]:my-5
+            lg:flex-row mb-[20vh]">
+            <TitleText />
+            <div className="flex flex-col items-center justify-center [&>*]:my-5 lg:ml-10">
+                <Image />
+                <MediaLinks />
             </div>
-            {/* Image & Contact Links */}
-            <div className="flex flex-grow flex-col items-center my-0 mx-10
-                            md:justify-center md:flex-grow-0"
-            >
 
-                {/* Image Container */}
-                <SlideAppear className="flex justify-center mb-[5vh]" offsetX="20vw" offsetY="5vh" once={false}>
-
-                    <motion.div
-                        whileHover={{ scale: 1.1 }}
-                        whileTap={{ scale: 0.9 }}
-                        transition={{ type: "spring", damping: 20, stiffness: 200, mass: 2 }}>
-                        <div className="relative overflow-hidden rounded-3xl max-w-[200px] max-h-[200px] h-[60vw] w-[60vw] shadow-3xl
-                                    md:h-[30vw] md:w-[30vw] md:max-w-[500px] md:max-h-[500px]">
-                            {/* Image */}
-                            <img src="/portrait2.jpg" alt="Portrait of me at Figueroa Terrace and Centennial Street in L.A."
-                                className="h-full w-full object-cover"
-                            />
-                        </div>
-                    </motion.div>
-                </SlideAppear>
-
-                {/* Contact Links */}
-                <div className="flex justify-center w-[80%] h-fit z-20
-                                md:w-40">
-                    <a href="https://github.com/tranjm4" target="_blank"
-                        className="p-1 mx-5 flex justify-center rounded-xl duration-300 hover:bg-dark-700">
-                        <img src={githubLogo} alt="Github Logo"
-                            className="w-[20vw] max-w-[100px] lg:max-w-[120px] xl:max-w-[150px]" />
-                    </a>
-                    <a href="https://www.linkedin.com/in/jonathan-tran-b405301ba/" target="_blank"
-                        className="px-4 mx-5 flex justify-center rounded-xl duration-300 hover:bg-dark-700">
-                        <img src={linkedinLogo} alt="LinkedIn Logo"
-                            className="w-[15vw] max-w-[80px] lg:max-w-[100px] xl:max-w-[120px]" />
-                    </a>
-                </div>
-            </div>
-        </div >
+        </div>
     )
 }
 
