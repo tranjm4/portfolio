c0e4f46cff927db9aa036ca64005f78a62988daa
tranjm4 <98729052+tranjm4@users.noreply.github.com>
2023-09-07 07:33:02 -0700

    Added Nav bar, refactored existing components to fit

--- src/App.tsx
+++ src/App.tsx
@@ -7,32 +7,110 @@ import Intro from './components/Intro/Intro';
 import Skills from './components/Skills/Skills';
 import { useEffect, useRef } from 'react';
 import circle from "./assets/circle.svg";
-import { motion, useAnimation, useInView } from 'framer-motion';
+import { AnimationControls, motion, useAnimation, useInView } from 'framer-motion';
 import Projects from './components/Projects/Projects';
 import Nav from './components/Nav/Nav';
 
-
 function App() {
+  const titleRef = useRef(null);
+  const introRef = useRef(null);
+  const skillsRef = useRef(null);
+  const projectsRef = useRef(null);
+
+  // const openInView = useInView(openRef, { once: false });
+  const titleInView = useInView(titleRef, { once: false });
+  const introInView = useInView(introRef, { once: false });
+  const skillsInView = useInView(skillsRef, { once: false });
+  const projectsInView = useInView(projectsRef, { once: false });
+
+  const mainControls = useAnimation();
+
+  useEffect(() => {
+    if (titleInView) {
+      mainControls.start("title");
+    }
+    else if (introInView) {
+      mainControls.start("intro");
+    }
+    else if (skillsInView) {
+      mainControls.start("skills");
+    }
+    else if (projectsInView) {
+      mainControls.start("projects");
+    }
+  }, [titleInView, introInView, skillsInView, projectsInView])
+
   return (
-    <div className="flex flex-col">
-      <Nav />
-      <section id="Title">
-        <Title />
-      </section>
+    <>
+      <Circles mainControls={mainControls} />
+      <div className="flex flex-col w-screen relative z-20 overflow-hidden
+        lg:flex-row">
+        <Nav />
+        <div className="">
+          <section ref={titleRef} id="Title">
+            <Title />
+          </section>
 
-      <section id="Intro">
-        <Intro />
-      </section>
+          <section ref={introRef} id="Intro">
+            <Intro />
+          </section>
 
-      <section id="Skills">
-        <Skills />
-      </section>
+          <section ref={skillsRef} id="Skills">
+            <Skills />
+          </section>
 
-      <section id="Projects">
-        <Projects />
-      </section>
+          <section ref={projectsRef} id="Projects">
+            <Projects />
+          </section>
+        </div>
 
-    </div>
+      </div>
+    </>
+  )
+}
+
+interface CircleProps {
+  mainControls: AnimationControls;
+}
+
+const Circles = ({ mainControls }: CircleProps) => {
+  const transition = {
+    type: "spring",
+    damping: 40,
+    stiffness: 120,
+    restDelta: 0.001
+  };
+  return (
+    <>
+      <motion.div className="fixed top-1/2 left-1/2"
+        variants={{
+          open: { opacity: 0, translateX: "-10vw", translateY: "20vh" },
+          title: { opacity: 1, translateX: "5vw", translateY: 0, scale: 1 },
+          intro: { opacity: 1, translateX: "-10vw", translateY: "-10vh", scale: 1.2 },
+          skills: { opacity: 1, translateX: "15vw", translateY: 0, scale: 1.5 },
+          projects: { opacity: 1, translateX: 0, translateY: "10vh", scale: 1.2 },
+        }}
+        initial="open"
+        animate={mainControls}
+        transition={transition}
+      >
+        <img src={circle} className="scale-[3] md:scale-[3] opacity-[25%] -z-20" />
+      </motion.div>
+      <motion.div className="fixed top-1/2 left-1/2"
+        variants={{
+          open: { opacity: 0, translateX: "-5vw", translateY: "-5vh" },
+          title: { opacity: 1, translateX: "2vw", translateY: 0, scale: 1 },
+          intro: { opacity: 1, translateX: "8vw", translateY: "4vh", scale: 1.2 },
+          skills: { opacity: 1, translateX: "-5vw", translateY: 0, scale: 0.8 },
+          projects: { opacity: 1, translateX: 0, translateY: "-5vh", scale: 1 }
+        }}
+        initial="open"
+        animate={mainControls}
+        transition={transition}
+      >
+        <img src={circle} className="scale-[5] md:scale-[6] opacity-[15%] -z-20" />
+      </motion.div>
+    </>
   )
 }
 
